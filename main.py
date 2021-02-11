9bb6ac889c385642224b502c3f3e71775bbf408d
Jeeedz <44287948+naiguncha@users.noreply.github.com>
2021-02-11 21:13:32 +0700

    git init

---
+++ main.py
@@ -0,0 +1,46 @@
+import discord
+import time
+from snscrape.modules.twitter import TwitterSearchScraper
+from datetime import date, timedelta
+
+TOKEN = "BOT_TOKEN_HERE"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    exist_tweet_id = []
+    print('We have logged in as {0.user}'.format(client))
+    await client.guilds[0].text_channels[0].send("Hi I'm Elon Musk Dogecoin bot back to ready state now!")
+    
+    while(True):
+        print("Checking Elon Musk Tweet!")
+        today = date.today()
+        yesterday = today - timedelta(days=1)
+        query = 'from:elonmusk since:'+str(yesterday)
+        tweets = TwitterSearchScraper(query).get_items()
+        for i, tweet in enumerate(tweets):
+            if i > 10:
+                break
+            content = tweet.content
+            tweet_id = tweet.id
+            if ('doge' in content.lower() and tweet_id not in exist_tweet_id):
+                print(content)
+                print(tweet_id)
+                print('=============================================================')
+                exist_tweet_id.append(tweet_id)
+                await client.guilds[0].text_channels[0].send("Seem like this tweet involve with Dogecoin check it out! @everyone\n"+str(tweet))
+        
+        print("Checking Elon Musk Tweet Done sleeping for 60 second")
+        time.sleep(60)
+            
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+if __name__ == '__main__':
+    client.run(TOKEN)
